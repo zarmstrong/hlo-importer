ad80c0071b0dc3812005ded6a58a6f98b2bdad47
zarmstrong <zarmstrong@traffid.com>
2021-06-30 22:49:35 -0600

    update module.json for 0.7.2

--- hlo-importer.js
+++ hlo-importer.js
@@ -1,5 +1,5 @@
 let hlodebug = false;
-const hloiVer="0.6.1";
+const hloiVer="0.7.2";
 
 const color1='color: #7bf542';  //bright green
 const color2='color: #d8eb34'; //yellow green
@@ -77,6 +77,13 @@ Hooks.on('renderActorSheet', function(obj, html){
 
 async function doHVExport(hero,act) {
   game.modules.get('herovaultfoundry')?.api?.exportToHVFromPBHLO(hero,act);
+  // if (game.modules.get('herovaultfoundry')?.active){
+  //   let {exportToHVFromPBHLO} = await import('../herovaultfoundry/herovault-min.js');
+  //   if (typeof exportToHVFromPBHLO !== "undefined") {
+  //     exportToHVFromPBHLO(hero,act);
+  //   }
+  // }  
+  // return;
 }
 
 export function hloShim(targetActor) {
